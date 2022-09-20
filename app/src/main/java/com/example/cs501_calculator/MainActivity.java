4de825fe4ea0caf422317b5b33d0757529e81461
Byeong Heon Ahn <ahnbh@bu.edu>
2022-09-20 01:35:50 -0400

    initial commit

---
+++ app/src/main/java/com/example/cs501_calculator/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.cs501_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvAnswer;
+    private Button num0;
+    private Button num1;
+    private Button num2;
+    private Button num3;
+    private Button num4;
+    private Button num5;
+    private Button num6;
+    private Button num7;
+    private Button num8;
+    private Button num9;
+    private Button Dec;
+    private Button Add;
+    private Button Sub;
+    private Button Mul;
+    private Button Div;
+    private Button Sqrt;
+    private Button eq;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvAnswer = (TextView) findViewById(R.id.tvAnswer);
+        num0 = (Button) findViewById(R.id.num0);
+        num1 = (Button) findViewById(R.id.num1);
+        num2 = (Button) findViewById(R.id.num2);
+        num3 = (Button) findViewById(R.id.num3);
+        num4 = (Button) findViewById(R.id.num4);
+        num5 = (Button) findViewById(R.id.num5);
+        num6 = (Button) findViewById(R.id.num6);
+        num7 = (Button) findViewById(R.id.num7);
+        num8 = (Button) findViewById(R.id.num8);
+        num9 = (Button) findViewById(R.id.num9);
+        Dec = (Button) findViewById(R.id.Dec);
+        Add = (Button) findViewById(R.id.Add);
+        Sub = (Button) findViewById(R.id.Sub);
+        Mul = (Button) findViewById(R.id.Mul);
+        Div = (Button) findViewById(R.id.Div);
+        Sqrt = (Button) findViewById(R.id.Sqrt);
+        eq = (Button) findViewById(R.id.eq);
+
+
+
+    }
+}
